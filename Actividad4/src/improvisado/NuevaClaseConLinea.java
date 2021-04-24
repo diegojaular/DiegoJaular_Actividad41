cb4c194c69c146882e66dec1a83ff2c9e6b60a72
diego <diegordesdecasa.es>
2021-04-24 13:32:21 +0200

    2/2 nueva branch

--- Actividad4/src/improvisado/NuevaClaseConLinea.java
+++ Actividad4/src/improvisado/NuevaClaseConLinea.java
@@ -5,6 +5,7 @@ public class NuevaClaseConLinea {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Linea con nueva branhc?");
+		System.out.println("Hay que hacer 2 mas, aqui va el 1");
 	}
 
 }
