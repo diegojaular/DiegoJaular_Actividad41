007b62c6fd908263e09775fd31a03e247bac043a
diego <diegordesdecasa.es>
2021-04-24 13:22:40 +0200

    commit y push pa

--- Actividad4/src/improvisado/ClaseParaHacerUnCommit.java
+++ Actividad4/src/improvisado/ClaseParaHacerUnCommit.java
@@ -4,7 +4,8 @@ public class ClaseParaHacerUnCommit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola commit dejame pushear"
+				+ "que si no no puedo");
 	}
 
 }
