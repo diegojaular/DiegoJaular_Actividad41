0fc7b498c0d8d4ec27fb312bba2301baa3171f02
diego <diegordesdecasa.es>
2021-04-24 13:31:13 +0200

    otra mas, en local, nueva branch

--- Actividad4/src/improvisado/OtraClaseNueva.java
+++ Actividad4/src/improvisado/OtraClaseNueva.java
@@ -1,10 +1,10 @@
 package improvisado;
 
-public class NuevaClaseConLinea {
+public class OtraClaseNueva {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Linea con nueva branhc?");
+
 	}
 
 }
